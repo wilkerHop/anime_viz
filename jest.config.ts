94b5f59e1836eb642caf0839e263c6e2ff5ee901
Wilker Ribeiro <rekliw97@gmail.com>
2025-11-25 15:16:15 -0300

    chore: add e2e directory to Jest test path ignore patterns

--- jest.config.ts
+++ jest.config.ts
@@ -15,6 +15,7 @@ const config: Config = {
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1',
   },
+  testPathIgnorePatterns: ['<rootDir>/e2e/'],
 }
  
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
